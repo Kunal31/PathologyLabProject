a995343e8bb4a8452ad3107ca2e3b721d88ab1f4
aspade <aspade@aspade.(none)>
2015-05-03 12:39:13 +0530

    screenshot_taken

--- PathologyLabProject/settings.py
+++ PathologyLabProject/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/home/aspade/workspace/PathologyLabProject/labmanagement/static/templates/'
+STATIC_URL = '/home/aspade/workspace/PathologyLabProject/labmanagement/static/'
 
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, '../templates/css/'),
